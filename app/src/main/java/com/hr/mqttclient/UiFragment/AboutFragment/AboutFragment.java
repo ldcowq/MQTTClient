49ca2d51facd17010a91944d27682b7ac11c4950
ldcowq <1057745451@qq.com>
2020-11-08 22:39:27 +0800

    First commit.

---
+++ app/src/main/java/com/hr/mqttclient/UiFragment/AboutFragment/AboutFragment.java
@@ -0,0 +1,31 @@
+package com.hr.mqttclient.UiFragment.AboutFragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.hr.mqttclient.R;
+
+public class AboutFragment extends Fragment {
+
+    private TextView developerName_textview;
+    private TextView email;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.about, container, false);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+}
